753e42562c9ad91e1b826eab630210ec1429cee4
valdirapn <valdirneto.e-deploy@natura.net>
2022-08-29 14:34:09 -0300

    initial commit

---
+++ ibeauty/src/main/kotlin/br/com/vpn/ibeauty/IBeautyApplication.kt
@@ -0,0 +1,11 @@
+package br.com.vpn.ibeauty
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IBeautyApplication
+
+fun main(args: Array<String>) {
+	runApplication<IBeautyApplication>(*args)
+}
---
+++ ibeauty/src/test/kotlin/br/com/vpn/ibeauty/IBeautyApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.vpn.ibeauty
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IBeautyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
